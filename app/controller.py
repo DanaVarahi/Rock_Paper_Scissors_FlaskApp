aa6652a4df639702bd77b08ef6f16a05b8ef28fd
Dana Varahi <dana_varahi@protonmail.com>
2020-11-01 17:46:28 +0000

    Refactored controller

--- app/controller.py
+++ app/controller.py
@@ -26,27 +26,29 @@ def players(choice1=None,choice2=None):
         message_body = f"{winner.name} has won with {winner.choice}."
         return render_template('result.html',heading=message_title, description=message_body)
 
-@app.route('/play', methods=['GET'] )
-def single_player_get():
-  return render_template('play.html')
+@app.route('/play', methods = ['GET', 'POST'])
 
+def single_player():
+  if request.method == 'GET':
+    return render_template('play.html')
 
-@app.route('/play', methods=['POST'] )
-def single_player_post():
-  players_name = request.form['name']
-  players_choice = request.form['choice']
-  player1 = Player(players_name, players_choice)
-  player2 = Player('','')
-  game = Game(player1, player2)
-  game.set_computer_player()   
-  winner = game.return_winner()
-  if winner is None:
-    message_title = "It's a draw."
-    message_body = f"{game.player1.name} and {game.player2.name} both chose {game.player2.choice}." 
-      
-    return render_template('result.html', heading=message_title,description=message_body)
-      
   else:
+    players_name = request.form['name']
+    players_choice = request.form['choice']
+
+    player1 = Player(players_name, players_choice)
+    player2 = Player('','')
+
+    game = Game(player1, player2)
+    game.set_computer_player()   
+    winner = game.return_winner()
+
+    if winner is None:
+      message_title = "It's a draw."
+      message_body = f"{game.player1.name} and {game.player2.name} both chose {game.player2.choice}." 
+      return render_template('result.html', heading=message_title,description=message_body)
+      
+    else:
       message_title = f"{winner.name} is the winner!"
       message_body = f"{winner.name} has won with {winner.choice}."
       return render_template('result.html',heading=message_title, description=message_body)
